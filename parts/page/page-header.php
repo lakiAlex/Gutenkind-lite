b3606dbd25352c61f2ac6b054c90817491679e04
Lazar Momcilovic <vossendesign@gmail.com>
2019-09-12 08:25:07 +0200

    Refactoring

--- parts/page/page-header.php
+++ parts/page/page-header.php
@@ -4,6 +4,7 @@ $page_header	= get_theme_mod('page-header', true);
 $title			= get_theme_mod('page-header-title', true);
 
 if (is_page() && !is_front_page() && !is_404() && $page_header == true) { ?>
+
 	<div class="page-header-full">
 		<div class="page-header" data-bg="<?php echo esc_url(gutenkind_thumb_url('fullscreen')); ?>">
 			<?php if ($title == true) { ?>
@@ -11,4 +12,5 @@ if (is_page() && !is_front_page() && !is_404() && $page_header == true) { ?>
 			<?php } ?>
 		</div>
 	</div> <?php
+	
 }
